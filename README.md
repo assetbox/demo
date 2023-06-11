2574455c72033b38c26b6d5223108eb261160c8b
gronxb <gron1gh1@gmail.com>
2023-06-12 04:00:19 +0900

    setup

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# demo
+# @assetbox/demo
+
+This is a demo of the `assetbox static-build` and `assetboxicon-build`.
+
+https://assetbox.github.io/docs/icon-build/svg-to-react-component
+
+### Example icon used\*\*
+
+[Ant Design Icons](https://github.com/ant-design/ant-design-icons) | [MIT](https://opensource.org/licenses/MIT)
---
+++ assetbox.config.cjs
@@ -0,0 +1,17 @@
+const { react } = require("@assetbox/plugin-react-icon");
+
+/**
+ * @type {import('@assetbox/cli').AssetBoxScheme}
+ */
+const config = {
+  categories: {
+    "antd-filled": ["./antd/filled/**/*"],
+    "antd-outlined": ["./antd/outlined/**/*"],
+    "antd-twotone": ["./antd/twotone/**/*"],
+  },
+  iconBuild: {
+    plugins: [react()],
+  },
+};
+
+module.exports = config;
